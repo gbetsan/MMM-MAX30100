b51fe161a856af802bea8afcb4e52cfa13eaab7b
Greg Betsan <g.betsan@gmail.com>
2022-03-18 18:33:28 -0400

    Implement I2C
    Add I2C config

--- read_sensor.py
+++ read_sensor.py
@@ -1,9 +1,10 @@
 import sys 
 import os
+import smbus
 sys.path.append(os.path.abspath("lib/max30100"))
 import max30100
 
 
-mx30 = max30100.MAX30100()
+mx30 = max30100.MAX30100(i2c=smbus.SMBus(1))
 mx30.read_sensor()
 print("%d, %d".format(mx30.ir, mx30.red))
